b079ab70e02af14bc04929d3e612fb8e7974220c
Ben Franzi <me@benfranzi.com>
2024-05-05 09:04:07 +1000

    feature: add testing

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import styles from './styles.module.css';
 
 type Props = {
@@ -6,7 +6,11 @@ type Props = {
 }
 
 export default function Title({ value }: Props): ReactNode {
+  const [count, setCount] = useState(0);
   return (
-    <h1 className={ styles.title }>{value}</h1>
+    <div>
+      <h1 className={ styles.title }>{value}</h1>
+      <button data-testid='counter' onClick={ () => setCount(count + 1) }>I have been clicked {count} times!</button>
+    </div>
   );
 }
