5851718530f09ab9f4947a5ea864c73f7c3c8649
Ben Franzi <BenFranzi@users.noreply.github.com>
2025-04-07 19:46:07 +1000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -67,6 +67,15 @@ export default defineConfig([
       'space-unary-ops': 'error',
       'yoda': 2,
       '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'args': 'all',
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_',
+        },
+      ],
     },
   },
 ]);
